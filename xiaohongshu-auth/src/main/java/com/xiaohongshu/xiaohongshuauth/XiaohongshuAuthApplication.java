c05c4858c0ff648b5f1c7172b25915524e16fc86
guohongwei <weihong010925@163.com>
2025-08-19 11:11:23 +0800

    chore(xhs):小宏书项目构建 1、创建小宏书项目、添加Spring Cloud Alibaba微服务框架 2、创建auth认证服务（负责处理用户登录、注册、账号注销等） 3、打包、运行测试

---
+++ xiaohongshu-auth/src/main/java/com/xiaohongshu/xiaohongshuauth/XiaohongshuAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohongshu.xiaohongshuauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohongshuAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaohongshuAuthApplication.class, args);
+    }
+
+}
---
+++ xiaohongshu-auth/src/test/java/com/xiaohongshu/xiaohongshuauth/XiaohongshuAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohongshu.xiaohongshuauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaohongshuAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
