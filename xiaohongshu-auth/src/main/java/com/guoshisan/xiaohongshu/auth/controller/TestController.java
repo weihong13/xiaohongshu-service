66ef3969d3e7816ef9dbfd9f2876116f19a30e7f
guohongwei <weihong010925@163.com>
2026-08-20 21:41:31 +0800

    feat: 整合 SaToken 权限认证框架，以及初步尝鲜

--- xiaohongshu-auth/src/main/java/com/guoshisan/xiaohongshu/auth/controller/TestController.java
+++ xiaohongshu-auth/src/main/java/com/guoshisan/xiaohongshu/auth/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.guoshisan.xiaohongshu.auth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.guoshisan.framework.jackson.framework.biz.operationlog.aspect.ApiOperationLog;
 import com.guoshisan.framework.common.response.Response;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: GHW
@@ -28,6 +26,23 @@ public class TestController {
     public Response<User> test2(@RequestBody @Validated User user) {
          int i = 1/ 0;
         return Response.success(user);
+    }
+
+    // 测试登录，浏览器访问： http://localhost:8080/user/doLogin?username=zhang&password=123456
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8080/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
     }
 }
 
